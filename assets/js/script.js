5109a986ce7d489cc77851111de88cbc986162c3
AntonLundd <anton.lundberg1337@gmail.com>
2023-01-15 00:25:16 +0000

    Add event listener for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,18 @@ const computerSelected = document.getElementById('computer-selected');
 const winnerDisplay = document.getElementById('winner-display');
 const scoreGreen = document.getElementById('score-green');
 const scoreRed = document.getElementById('score-red');
+const buttons = document.getElementsByClassName('action');
 
 const choices = ['Rock', 'Paper', 'Scissors'];
 let playerScore = 0;
 let computerScore = 0;
+
+// Add event listener to buttons
+for (let i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', playRound);
+}
+
+// Function that will be called when a button is clicked
+function playRound(event) {
+    
+}
