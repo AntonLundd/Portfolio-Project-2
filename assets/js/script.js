b9c562d0a0366fbefafb328f913b193090c3662b
AntonLundd <anton.lundberg1337@gmail.com>
2023-01-15 00:17:06 +0000

    Add global constants

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const playerSelected = document.getElementById('player-selected');
+const computerSelected = document.getElementById('computer-selected');
+const winnerDisplay = document.getElementById('winner-display');
+const scoreGreen = document.getElementById('score-green');
+const scoreRed = document.getElementById('score-red');
+
+const choices = ['Rock', 'Paper', 'Scissors'];
+let playerScore = 0;
+let computerScore = 0;
